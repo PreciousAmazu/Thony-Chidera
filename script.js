7a002a43e880b05880bd67441b7343dda705758b
Preciousgift Amazu <amazuchizurumoke@gmail.com>
2024-09-09 12:41:10 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+function toggleNav() {
+    var nav = document.getElementById("side-nav");
+    if (nav.style.left === "-250px") {
+        nav.style.left = "0";
+    } else {
+        nav.style.left = "-250px";
+    }
+}
+
+let currentImageIndex = 0;
+const images = [
+    './images/image2.jpg',
+    './images/image3.jpg',
+    './images/image4.jpg'
+];
+
+function changeHeroImage() {
+    const heroElement = document.querySelector('.hero');
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    heroElement.style.backgroundImage = `url(${images[currentImageIndex]})`;
+}
+
+setInterval(changeHeroImage, 15000);
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+  .addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    btn.value = 'Sending...';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_e10bs9s';
+
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Send Email';
+        alert('Sent!');
+      }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+      });
+  });
